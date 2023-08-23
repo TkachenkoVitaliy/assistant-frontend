83e77862e1c902dd91c10d736e8c8c1f33b58e02
Vitaliy Tkachenko <tkachenko.java@yandex.ru>
2023-08-23 17:06:44 +0500

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo 'start deploy'
+echo 'remove docker containers and images'
+docker rm --force front
+docker system prune -f
+echo 'pull last version from gitlab'
+git pull
+echo 'build docker image'
+docker build -t frontend .
+echo 'run docker container'
+docker run -d -it -p 80:80 --restart always --name front frontend
+echo 'deploy completed'
