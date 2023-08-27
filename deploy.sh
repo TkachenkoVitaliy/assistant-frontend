af4bb4367ee9f589af3fb15a5aec07239e3ed3fb
TkachenkoVitaliy <tkachenko.java@yandex.ru>
2023-08-27 22:06:29 +0500

    rename branches

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ docker run -d -it -p 80:80 --restart always --name front frontend
 echo 'connect container to dockernet network'
 docker network inspect dockernet >/dev/null 2>&1 || docker network create --driver bridge dockernet
 docker network connect dockernet front
-echo 'complete!!!'
+echo 'FRONTEND complete!!!'
